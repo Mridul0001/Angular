950c8deaa4a9208dd1ea1fd4e0963d5981e282d1
Mridul0001 <mridulmahajan0001@gmail.com>
2020-09-15 11:50:48 +0530

    Learned *ngFor. Awesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,5 @@ import { HelloComponent } from './hello/hello.component';
 })
 
 export class AppComponent {
-  text = "";
-
-  constructor(){}
-
-  updateValue(event) {
-    this.text = event.target.value;
-    console.log(event.target.value);
-  }
+  records = ["Apple", "Orange", "Mango", "Banana"]
 }
